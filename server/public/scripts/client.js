9534a694145f5be57273a2c71977d30b5e1ad4b7
Sarah Harrington <sarah.soberg@gmail.com>
2018-07-15 15:50:51 -0500

    initial board layout and pieces placed for each player

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,4 +1,24 @@
 console.log('javascript loaded');
 
-const board = document.querySelector('.board');
+const boardRows = [...document.querySelectorAll('.board-row')];
+const boardSpaces = [...document.querySelectorAll('.playable')];
+
+function gameStart() {
+  //functions for loading initial piece placement
+}
+
+for (let i = 0; i <= 11; i++) {
+  let playerOnePiece = document.createElement('div');
+  playerOnePiece.classList.add('player-one-piece');
+  boardSpaces[i].appendChild(playerOnePiece);
+}
+
+for (let i = 20; i <= 32; i++) {
+  let playerTwoPiece = document.createElement('div');
+  playerTwoPiece.classList.add('player-two-piece');
+  boardSpaces[i].appendChild(playerTwoPiece);
+}
+
+console.log('boardRows', boardRows);
+console.log('boardSpaces', boardSpaces);
 
